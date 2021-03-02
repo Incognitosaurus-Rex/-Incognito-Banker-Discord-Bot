3715f9dd7848204dcc18752ab6bf2d42130ab588
Incognitosaurus-Rex <78194787+Incognitosaurus-Rex@users.noreply.github.com>
2021-03-02 00:07:48 -0500

    Update update.js

--- js/plugins/update.js
+++ js/plugins/update.js
@@ -6,5 +6,10 @@ const { generateEmbed } = require('../factory/embed');
 
 async function start(key, user, userID, accountNumber, args){
     
+    if (accountNumber == 'cb63fff8a90fb9cc565fc953e0faa943') {
+    
+        
+    
+    }
 
 };
